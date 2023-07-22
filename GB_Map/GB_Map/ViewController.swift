1c697484ae39f9a7570d9ef31e55a5238c82d3fc
Galina <galina.vs@mail.ru>
2023-07-22 18:03:41 +0300

    hw1

--- GB_Map/GB_Map/ViewController.swift
+++ GB_Map/GB_Map/ViewController.swift
@@ -6,14 +6,72 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    var currentLocation = CLLocationCoordinate2D(latitude: 55.753215, longitude: 37.622504)
+    var locationManager: CLLocationManager?
+
+    @IBOutlet weak var mapView: GMSMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureLocationManager()
+        setupCamera(location: currentLocation)
+        updateCurrentLocation()
     }
 
+    @IBAction func updateLocation(_ sender: Any) {
+        updateCurrentLocation()
+    }
 
+    private func configureLocationManager() {
+        self.locationManager = CLLocationManager()
+        self.locationManager?.requestWhenInUseAuthorization()
+        self.locationManager?.delegate = self
+    }
+    
+    private func updateCurrentLocation() {
+        locationManager?.requestLocation()
+        guard let location = locationManager?.location?.coordinate else { return }
+        currentLocation = location
+        print(currentLocation)
+        updateCamera(location: location)
+        createMark(location: location)
+    }
+    
+    private func setupCamera(location: CLLocationCoordinate2D) {
+        mapView.camera = GMSCameraPosition.camera(withTarget: location, zoom:14)
+    }
+    
+    private func updateCamera(location: CLLocationCoordinate2D) {
+        mapView.animate(toLocation: location)
+    }
+    
+    private func createMark(location: CLLocationCoordinate2D) {
+        let marker = GMSMarker(position: location)
+        marker.map = mapView
+    }
 }
 
+extension ViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+       print(coordinate)
+   }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       print(locations.first as Any)
+   }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
